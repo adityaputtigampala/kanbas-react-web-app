68db803ed6f0e9d2b3d84e1d28a680e04d7e35f8
adityaputtigampala <aditya.puttigampala@gmail.com>
2024-05-20 17:57:01 -0400

    Assignment 1 updates

---
+++ src/Kanbas/Dashboard/index.tsx
@@ -0,0 +1,114 @@
+export default function Dashboard() {
+    return (
+      <div id="wd-dashboard">
+        <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
+        <h2 id="wd-dashboard-published">Published Courses (12)</h2> <hr />
+        <div id="wd-dashboard-courses">
+          <div className="wd-dashboard-course">
+            <img src="/images/reactjs.jpg" width={200} />
+            <div>
+              <a className="wd-dashboard-course-link"
+                href="#/Kanbas/Courses/1234/Home">
+                CS1234 React JS
+              </a>
+              <p className="wd-dashboard-course-title">
+                Full Stack software developer
+              </p>
+              <a href="#/Kanbas/Courses/1234/Home"> Go </a>
+            </div>
+          </div>
+          <div className="wd-dashboard-course">
+            <img src="/images/cooking.jpg" width={200} />
+            <div>
+              <a className="wd-dashboard-course-link"
+                href="#/Kanbas/Courses/1235/Home">
+                CS1235 Cooking
+              </a>
+              <p className="wd-dashboard-course-title">
+                Cooking Class
+              </p>
+              <a href="#/Kanbas/Courses/1235/Home"> Go </a>
+            </div>
+          </div>
+          <div className="wd-dashboard-course">
+            <img src="/images/swimming.jpg" width={200} />
+            <div>
+              <a className="wd-dashboard-course-link"
+                href="#/Kanbas/Courses/1236/Home">
+                CS1236 Swimming
+              </a>
+              <p className="wd-dashboard-course-title">
+                Swimming Class
+              </p>
+              <a href="#/Kanbas/Courses/1236/Home"> Go </a>
+            </div>
+          </div>
+          <div className="wd-dashboard-course">
+            <img src="/images/biking.jpg" width={200} />
+            <div>
+              <a className="wd-dashboard-course-link"
+                href="#/Kanbas/Courses/1237/Home">
+                CS1237 Biking
+              </a>
+              <p className="wd-dashboard-course-title">
+                Biking Class
+              </p>
+              <a href="#/Kanbas/Courses/1237/Home"> Go </a>
+            </div>
+          </div>
+          <div className="wd-dashboard-course">
+            <img src="/images/pingpong.jpg" width={200} />
+            <div>
+              <a className="wd-dashboard-course-link"
+                href="#/Kanbas/Courses/1238/Home">
+                CS1238 Ping Pong
+              </a>
+              <p className="wd-dashboard-course-title">
+                Ping Pong Class
+              </p>
+              <a href="#/Kanbas/Courses/1238/Home"> Go </a>
+            </div>
+          </div>
+          <div className="wd-dashboard-course">
+            <img src="/images/chess.jpg" width={200} />
+            <div>
+              <a className="wd-dashboard-course-link"
+                href="#/Kanbas/Courses/1239/Home">
+                CS1239 Chess
+              </a>
+              <p className="wd-dashboard-course-title">
+                Chess Class
+              </p>
+              <a href="#/Kanbas/Courses/1239/Home"> Go </a>
+            </div>
+          </div>
+          <div className="wd-dashboard-course">
+            <img src="/images/gardening.jpg" width={200} />
+            <div>
+              <a className="wd-dashboard-course-link"
+                href="#/Kanbas/Courses/1240/Home">
+                CS1240 Gardening
+              </a>
+              <p className="wd-dashboard-course-title">
+                Gardening Class
+              </p>
+              <a href="#/Kanbas/Courses/1240/Home"> Go </a>
+            </div>
+          </div>
+          <div className="wd-dashboard-course">
+            <img src="/images/reading.jpg" width={200} />
+            <div>
+              <a className="wd-dashboard-course-link"
+                href="#/Kanbas/Courses/1241/Home">
+                CS1241 Reading
+              </a>
+              <p className="wd-dashboard-course-title">
+                Reading Class
+              </p>
+              <a href="#/Kanbas/Courses/1241/Home"> Go </a>
+            </div>
+          </div>
+        </div>
+      </div>
+  );}
+  
